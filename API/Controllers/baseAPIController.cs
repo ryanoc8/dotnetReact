5d3dd373296474ddac484ae80d5377eda1defad5
ryan <ryan.oconnor3@mycit.ie>
2021-06-02 16:36:53 +0100

    initial commit

---
+++ API/Controllers/baseAPIController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class baseAPIController
+    {
+        public class BaseAPIController : ControllerBase
+        {
+
+        }
+    }
+}
